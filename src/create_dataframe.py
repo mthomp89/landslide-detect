31e65fc55551afc4390a3427ced2cf76f3f5c922
Thompson <mthompson1@riversideresearch.org>
2022-06-12 16:54:08 -0400

    revert back to ea292abcb01f97a99825782ff3d6e5505fb1088e -m 1

--- src/create_dataframe.py
+++ src/create_dataframe.py
@@ -1,10 +1,13 @@
-import pandas as pd
+import geopandas as gpd
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
 
 def create_dataframe(data_file):
     """
-    Reads a csv file into a Pandas DataFrame. Sets the index to the 'slide.id'
-    and sorts the DataFrame by the new index. Creates a new column 'point' by 
-    merging two other columns.
+    Reads a csv file into a GeoPandas DataFrame. Sets the index to the 'slide.id'
+    and sorts the geoDataFrame by the new index. Converts the date+time column to
+    a DateTime object and drops the time. Converts the reported geocoordinates to
+    a geoPandas geometry point.
 
     Parameters
     ----------
@@ -16,7 +19,14 @@ def create_dataframe(data_file):
         sorted by index
     """
 
-    df = pd.read_csv(data_file, index_col='slide.id').sort_values(by='slide.id')
-    df['point'] = df['lon'].map(str) + ', ' + df['lat'].map(str)
+    gdf = gpd.read_file(data_file, index_col='slide.id').sort_values(by='slide.id')
+    gdf = gdf.set_index('slide.id')
+    # Convert to DatTime object, drop time
+#     gdf['slide.date'] = pd.to_datetime(gdf['slide.date'])
+#     gdf['slide.date'] = gdf['slide.date'].dt.date
+    # Convert reported geocoordinates to geometery
+    gdf[['lon', 'lat']] = gdf[['lon', 'lat']].astype(float)
+    geometry = [Point(xy) for xy in zip(gdf.lon, gdf.lat)]
+    gdf = GeoDataFrame(gdf, crs="EPSG:4326", geometry=geometry)
 
-    return df
+    return gdf
